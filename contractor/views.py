cc024d7b50bfd0f0f385db1c248457a31bf4bba4
Kyle Slaugh <kyleeslaugh@gmail.com>
2020-06-24 22:25:11 -0700

    added contractor login page and started the registration page

--- contractor/views.py
+++ contractor/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contractors,Vehicles,Ratings,Reviews,Bids
+from django.contrib import messages
+import bcrypt
 
 def index(request):
     return render(request,'contractor.html')
+
+def log(request):
+    e=Contractors.objects.logVals(request.POST)
+    if len(e)==0:
+        request.session['user']=Contractors.objects.get(email=request.POST['e']).id
+        return redirect('/admin/home')
+    else:
+        for i in e.values():
+            messages.error(request,i)
+        return redirect('/admin/')
+
+def reg(request):
+    return render(request,'cont-reg.html')
